3cfc449f6ad3337521d465501c8761bdde4f7099
Ali-Nezamli <alinezamli54@gmail.com>
2026-01-28 18:42:29 +0400

    Initial Commit

---
+++ pass2pastios/pass2pastiosApp.swift
@@ -0,0 +1,32 @@
+//
+//  pass2pastiosApp.swift
+//  pass2pastios
+//
+//  Created by Ali Nezamli on 28/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct pass2pastiosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
